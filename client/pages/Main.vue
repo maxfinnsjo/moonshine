94aaa05f00c5e852ee4d3eb630ed450a1a46c6a2
maxfinnsjo <max.finnsjo@gmail.com>
2020-04-26 21:32:32 +0200

    Styled a few components :art:

--- client/pages/Main.vue
+++ client/pages/Main.vue
@@ -2,10 +2,10 @@
   <div>
     <div class="h-2 w-full bg-gray-700"></div>
     <div class="w-full h-12 flex items-center justify-between px-6">
-      <div class="text-pink-600">Moonshine</div>
+      <router-link to="/" class="text-pink-400 hover:text-pink-600">Moonshine</router-link>
       <div>
-        <router-link class="text-orange-500" to="/auth/login">Sign In</router-link>
-        <router-link class="text-orange-500 border-2 p-1 border-orange-500 ml-3" to="/auth/register">Register Account</router-link>
+        <router-link class="px-2 py-1 rounded-sm ml-3 bg-pink-200 text-gray-500 rounded-sm shadow focus:outline-none hover:text-gray-700 hover:bg-pink-400" to="/auth/login">Sign In</router-link>
+        <router-link class="px-2 py-1 rounded-sm ml-3 bg-pink-200 text-gray-500 rounded-sm shadow focus:outline-none hover:text-gray-700 hover:bg-pink-400" to="/auth/register">Register Account</router-link>
       </div>
     </div>
 
--- client/pages/Register.vue
+++ client/pages/Register.vue
@@ -1,5 +1,15 @@
 <template>
-  <h1>Register now!</h1>
+  <div class="container my-16 w-full mx-auto">
+    <div class="max-w-xs mx-auto">
+      <h1 class="text-center text-gray-600">Register</h1>
+      <div class="w-full bg-white shadow-lg mt-5 rounded p-8">
+        <input type="text" class="w-full bg-gray-100 p-2 focus:outline-none hover:bg-pink-100 focus:bg-pink-300 text-gray-700 text-xs mb-3" placeholder="Enter Username">
+        <input type="text" class="w-full bg-gray-100 p-2 focus:outline-none hover:bg-pink-100 focus:bg-pink-300 text-gray-700 text-xs mb-3" placeholder="Enter Email">
+        <input type="text" class="w-full bg-gray-100 p-2 focus:outline-none hover:bg-pink-100 focus:bg-pink-300 text-gray-700 text-xs" placeholder="Enter Password">
+        <button class="w-full text-sm mt-3 py-3 bg-pink-100 text-gray-500 rounded-sm shadow focus:outline-none hover:text-gray-700 hover:bg-pink-400">Send Registration</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
