802a63d0331d9b0dd2b1003f2efbd03be511505f
maxfinnsjo <max.finnsjo@gmail.com>
2020-04-26 22:00:26 +0200

    Created TextInput custom component :page_facing_up:

---
+++ client/components/TextInput.vue
@@ -0,0 +1,33 @@
+<template>
+  <input
+    type="text"
+    :value="value"
+    class="w-full bg-gray-100 p-2 focus:outline-none hover:bg-pink-100 focus:bg-pink-300 text-gray-700 text-xs mb-3"
+    :placeholder="placeholder"
+    @input="$emit('input', $event.target.value)"
+  />
+</template>
+
+<script>
+export default {
+  props: {
+    placeholder: {
+      type: String,
+      required: true
+    },
+    type: {
+      type: String,
+      required: false,
+      default: "text"
+    },
+    value: {
+      type: String,
+      required: false,
+      default: ""
+    }
+  }
+};
+</script>
+
+<style>
+</style>
--- client/pages/Register.vue
+++ client/pages/Register.vue
@@ -3,17 +3,27 @@
     <div class="max-w-xs mx-auto">
       <h1 class="text-center text-gray-600">Register</h1>
       <div class="w-full bg-white shadow-lg mt-5 rounded p-8">
-        <input type="text" class="w-full bg-gray-100 p-2 focus:outline-none hover:bg-pink-100 focus:bg-pink-300 text-gray-700 text-xs mb-3" placeholder="Enter Username">
-        <input type="text" class="w-full bg-gray-100 p-2 focus:outline-none hover:bg-pink-100 focus:bg-pink-300 text-gray-700 text-xs mb-3" placeholder="Enter Email">
-        <input type="text" class="w-full bg-gray-100 p-2 focus:outline-none hover:bg-pink-100 focus:bg-pink-300 text-gray-700 text-xs" placeholder="Enter Password">
-        <button class="w-full text-sm mt-3 py-3 bg-pink-100 text-gray-500 rounded-sm shadow focus:outline-none hover:text-gray-700 hover:bg-pink-400">Send Registration</button>
+        <text-input :value="model.name" v-model="model.name" placeholder="Enter Username" />
+        <text-input :value="model.email" v-model="model.email" placeholder="Enter Email" />
+        <text-input :value="model.password" v-model="model.password" placeholder="Enter Password" />
+        <button
+          class="w-full text-sm mt-3 py-3 bg-pink-100 text-gray-500 rounded-sm shadow focus:outline-none hover:text-gray-700 hover:bg-pink-400"
+        >Send Registration</button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data: () => ({
+    model: {
+      name: "",
+      email: "",
+      password: ""
+    }
+  })
+};
 </script>
 
 <style>
