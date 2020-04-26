57d90d015465c9dffe3212890de56bae8fe65e56
maxfinnsjo <max.finnsjo@gmail.com>
2020-04-26 22:43:35 +0200

    Added some validation :microscope:

--- client/components/TextInput.vue
+++ client/components/TextInput.vue
@@ -1,11 +1,18 @@
 <template>
-  <input
-    type="text"
-    :value="value"
-    class="w-full bg-gray-100 p-2 focus:outline-none hover:bg-pink-100 focus:bg-pink-300 text-gray-700 text-xs mb-3"
-    :placeholder="placeholder"
-    @input="$emit('input', $event.target.value)"
-  />
+  <div class="mb-3">
+    <input
+      type="text"
+      :name="name"
+      :value="value"
+      @focus="$emit('fo cus')"
+      @blur="$emit('blur')"
+      :placeholder="placeholder"
+      @change="$emit('input', $event.target.value)"
+      @input="$emit('input', $event.target.value)"
+      class="w-full bg-gray-100 p-2 focus:outline-none hover:bg-pink-100 focus:bg-pink-300 text-gray-700 text-xs"
+    />
+    <span v-if="error" class="text-xs text-red-700">{{ error }}</span>
+  </div>
 </template>
 
 <script>
@@ -24,6 +31,14 @@ export default {
       type: String,
       required: false,
       default: ""
+    },
+    name: {
+      type: String,
+      required: true
+    },
+    error: {
+      type: String,
+      required: false
     }
   }
 };
--- client/pages/Register.vue
+++ client/pages/Register.vue
@@ -3,10 +3,33 @@
     <div class="max-w-xs mx-auto">
       <h1 class="text-center text-gray-600">Register</h1>
       <div class="w-full bg-white shadow-lg mt-5 rounded p-8">
-        <text-input :value="model.name" v-model="model.name" placeholder="Enter Username" />
-        <text-input :value="model.email" v-model="model.email" placeholder="Enter Email" />
-        <text-input :value="model.password" v-model="model.password" placeholder="Enter Password" />
+        <text-input
+          name="name"
+          :value="model.name"
+          v-model="model.name"
+          v-validate="'required'"
+          placeholder="Enter Username"
+          :error="errors.first('name')"
+        />
+        <text-input
+          name="email"
+          :value="model.email"
+          v-model="model.email"
+          v-validate="'required|email'"
+          placeholder="Enter Email"
+          :error="errors.first('email')"
+        />
+        <text-input
+          type="password"
+          name="password"
+          :value="model.password"
+          v-model="model.password"
+          v-validate="'required|min:6'"
+          :error="errors.first('password')"
+          placeholder="Enter Password"
+        />
         <button
+          @click="register"
           class="w-full text-sm mt-3 py-3 bg-pink-100 text-gray-500 rounded-sm shadow focus:outline-none hover:text-gray-700 hover:bg-pink-400"
         >Send Registration</button>
       </div>
@@ -22,7 +45,16 @@ export default {
       email: "",
       password: ""
     }
-  })
+  }),
+  methods: {
+    register() {
+      this.$validator.validate().then(isValid => {
+        if (!isValid) {
+          return;
+        }
+      });
+    }
+  }
 };
 </script>
 
